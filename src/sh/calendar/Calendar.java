f6981eb169a378c779e9cb9fc73643c15dfecc02
Sehyeong Kim <ksh349021@gmail.com>
2019-12-18 23:03:36 +0900

    Update to use class method

--- src/sh/calendar/Calendar.java
+++ src/sh/calendar/Calendar.java
@@ -1,27 +1,24 @@
 package sh.calendar;
 
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Calendar {
+	
+	private static final int[] MONTH_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int daysOfMonth(int month) {
+
+		return MONTH_DAYS[month - 1];
+		
+	}
 	public static void main(String[] args) {
-		int[] thirtyOne = new int[]{1, 3, 5, 7, 8, 10, 12};
-		int[] thirty = new int[]{4, 6, 9, 11};
-		int feb = 2;
 		
 		Scanner input = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("조회할 달을 입력해 주세요");
 		int num = input.nextInt();
 		
-		if(IntStream.of(thirtyOne).anyMatch(x -> x == num)) {
-			System.out.printf("%d월은 31일까지 있습니다\n", num);
-		} else if(IntStream.of(thirty).anyMatch(x -> x == num)) {
-			System.out.printf("%d월은 30일까지 있습니다\n", num);
-		} else if(num == feb) {
-			System.out.printf("%d월은 28일까지 있습니다\n", num);
-		} else {
-			System.out.printf("%d월은 존재하지 않습니다\n", num);
-		}
+		System.out.printf("%d월은 %d일까지 있습니다\n", num, cal.daysOfMonth(num));
 		
 		input.close();
 	}
