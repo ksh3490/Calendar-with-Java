f46bd4f4df302d783282db469938c6c2af6861fb
Sehyeong Kim <ksh349021@gmail.com>
2019-12-20 01:12:51 +0900

    Using "for" to implement loop feature.

--- src/sh/calendar/Calendar.java
+++ src/sh/calendar/Calendar.java
@@ -3,23 +3,31 @@ package sh.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MONTH_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MONTH_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int daysOfMonth(int month) {
 
 		return MONTH_DAYS[month - 1];
-		
+
 	}
+
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
+		Scanner monthInput = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("조회할 달을 입력해 주세요");
+		System.out.println("반복 횟수를 입력하세요");
 		int num = input.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다\n", num, cal.daysOfMonth(num));
-		
+		for (int i = 1; i <= num; i++) {
+			System.out.println("계산할 달을 입력하세요");
+			int month = monthInput.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.daysOfMonth(month));
+		}
+		System.out.println("설정한 반복 횟수가 끝났습니다");
+
 		input.close();
+		monthInput.close();
 	}
 }
