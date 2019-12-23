a8515ad075f921b894c0765d17a0cea755390c35
ksh3490 <ksh349021@gmail.com>
2019-12-23 23:53:21 +0900

    Added a method "getStartDay" to calculate the number of starting spaces

--- src/sh/calendar/Calendar.java
+++ src/sh/calendar/Calendar.java
@@ -1,10 +1,26 @@
 package sh.calendar;
 
+import java.lang.annotation.Repeatable;
+
 public class Calendar {
 
 	private static final int[] MONTH_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MONTH_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String[] WEEK_DAYS = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
+	public int getStartDay (String day) {
+		int startDay = -1;
+		
+		for (int i = 0; i < WEEK_DAYS.length; i++) {
+			if (WEEK_DAYS[i] == day) {
+				startDay = i;
+				break;
+			}
+		}
+		
+		return startDay;
+	}
+	
 	public boolean isLeapYear (int year) {
 		
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
@@ -14,16 +30,24 @@ public class Calendar {
 		}
 	}
 	
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, String day) {
 		
 		System.out.printf("    <<%4d년%3d월>>\n", year, month);
-		System.out.println("  일 월  화  수 목  금  토");
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println(" --------------------");
 		
-				
+		int startDay = getStartDay(day);		
 		int maxDay = daysOfMonth(year, month);
 		
+		int startSpaceNum = startDay * 3;
+		String repeatedSpace = "";
 		
+		for (int i = 1; i <= startSpaceNum; i++) {
+			repeatedSpace.concat(" ");
+		}
+		
+		
+		System.out.print(repeatedSpace);
 		
 		for(int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
--- src/sh/calendar/Prompt.java
+++ src/sh/calendar/Prompt.java
@@ -6,14 +6,17 @@ public class Prompt {
 
 	private final static String PROMPTYEAR = "YEAR> ";
 	private final static String PROMPTMONTH = "MONTH> ";
+	private final static String PROMPTDAY = "DAY> ";
 
 	public void runPrompt() {
 		Scanner yearInput = new Scanner(System.in);
 		Scanner monthInput = new Scanner(System.in);
+		Scanner dayInput = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int month = -1;
 		int year = -1;
+		String day = "";
 		while (true) {
 			System.out.println("출력하려는 연도를 입력하세요");
 			System.out.print(PROMPTYEAR);
@@ -21,6 +24,9 @@ public class Prompt {
 			System.out.println("출력하려는 달을 입력하세요");
 			System.out.print(PROMPTMONTH);
 			month = monthInput.nextInt();
+			System.out.println("시작하는 요일을 입력하세요");
+			System.out.print(PROMPTDAY);
+			day = dayInput.nextLine();
 
 			if (month == -1) {
 				System.out.println("달력 출력을 종료합니다");
@@ -37,12 +43,13 @@ public class Prompt {
 				continue;
 			}
 
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, day);
 
 		}
 		
 		yearInput.close();
 		monthInput.close();
+		dayInput.close();
 		
 	}
 
