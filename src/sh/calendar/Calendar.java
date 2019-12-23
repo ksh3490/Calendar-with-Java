0663e5db70948383f4995f0c11338bfdd734fc38
ksh3490 <ksh349021@gmail.com>
2019-12-24 00:18:26 +0900

    Completed the feature that changes the arrays of calendar with the day.

--- src/sh/calendar/Calendar.java
+++ src/sh/calendar/Calendar.java
@@ -1,72 +1,67 @@
 package sh.calendar;
 
-import java.lang.annotation.Repeatable;
-
 public class Calendar {
 
 	private static final int[] MONTH_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MONTH_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final String[] WEEK_DAYS = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
+	private static final int[] DAY_DIVIDER = { 7, 6, 5, 4, 3, 2, 1 };
 
-	public int getStartDay (String day) {
+	public int getStartDay(String day) {
 		int startDay = -1;
-		
+
 		for (int i = 0; i < WEEK_DAYS.length; i++) {
-			if (WEEK_DAYS[i] == day) {
+			if (WEEK_DAYS[i].equals(day)) {
 				startDay = i;
 				break;
 			}
 		}
-		
+
 		return startDay;
+
 	}
-	
-	public boolean isLeapYear (int year) {
-		
+
+	public boolean isLeapYear(int year) {
+
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	public void printCalendar(int year, int month, String day) {
-		
+
 		System.out.printf("    <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println(" --------------------");
-		
-		int startDay = getStartDay(day);		
+
+		int startDay = getStartDay(day);
 		int maxDay = daysOfMonth(year, month);
-		
-		int startSpaceNum = startDay * 3;
-		String repeatedSpace = "";
-		
-		for (int i = 1; i <= startSpaceNum; i++) {
-			repeatedSpace.concat(" ");
+		int dayDivider = DAY_DIVIDER[startDay];
+
+		for (int i = 1; i <= startDay; i++) {
+			System.out.print("   ");
 		}
-		
-		
-		System.out.print(repeatedSpace);
-		
-		for(int i = 1; i <= maxDay; i++) {
+
+		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			
-			if(i % 7 == 0) {
+
+			if (i == dayDivider || i == dayDivider + 7 ) {
 				System.out.println();
+				dayDivider = i;
 			}
 		}
-		
+
 		System.out.println();
-		
+
 	}
 
-	
 	public int daysOfMonth(int year, int month) {
-		
+
 		if (isLeapYear(year) == true) {
 			return LEAP_MONTH_DAYS[month - 1];
-		} else {			
+		} else {
 			return MONTH_DAYS[month - 1];
 		}
 
